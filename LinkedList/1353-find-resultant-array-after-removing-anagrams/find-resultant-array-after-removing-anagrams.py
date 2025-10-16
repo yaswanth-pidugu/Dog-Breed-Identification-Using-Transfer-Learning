e30305898dec25e0b9bc650188b251fb92b719ae
Yaswanth Pidugu <yashwanthpidugu999@gmail.com>
2025-10-16 14:03:26 +0530

    Delete LinkedList directory

--- LinkedList/1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def removeAnagrams(self, words: List[str]) -> List[str]:
-        return [next(g) for _, g in groupby(words, sorted)]
--- LinkedList/3336-water-bottles-ii/water-bottles-ii.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
-        res = numBottles
-        while numBottles >= numExchange:
-            numBottles -= numExchange
-            numExchange+=1
-            numBottles+=1
-            res+=1
-        return res
